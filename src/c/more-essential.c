3009a4397a3eb7cb808b916b1f1670cbf402a031
Jingbei Li <i@jingbei.li>
2016-11-04 23:08:04 +0800

    implemented basic layout

--- src/c/more-essential.c
+++ src/c/more-essential.c
@@ -23,14 +23,17 @@ static void main_window_load(Window* window) {
 	// Get information about the Window
 	Layer* window_layer = window_get_root_layer(window);
 	GRect bounds = layer_get_bounds(window_layer);
+
+	window_set_background_color(window, GColorVividCerulean);
+
 	// Create the TextLayer with specific bounds
 	s_time_layer = text_layer_create(
-	                   GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+	                   GRect(0, bounds.size.h/3, bounds.size.w, 50));
 	// Improve the layout to be more like a watchface
-	text_layer_set_background_color(s_time_layer, GColorClear);
+	text_layer_set_background_color(s_time_layer, GColorWhite);
 	text_layer_set_text_color(s_time_layer, GColorBlack);
 	text_layer_set_text(s_time_layer, "00:00");
-	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS));
 	text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 	// Add it as a child layer to the Window's root layer
 	layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
